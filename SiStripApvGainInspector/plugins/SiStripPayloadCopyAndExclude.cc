25d59dc792e6957d4714cd42f8f35532f15f8b88
mmusich <marco.musich@cern.ch>
2018-06-08 14:04:22 +0200

    add tool to mix Gain payload, by excluding modules from a user-provided list

---
+++ SiStripApvGainInspector/plugins/SiStripPayloadCopyAndExclude.cc
@@ -0,0 +1,238 @@
+// -*- C++ -*-
+//
+// Package:    SiStripMiscellanea/SiStripPayloadCopyAndExclude
+// Class:      SiStripPayloadCopyAndExclude
+//
+/**\class SiStripPayloadCopyAndExclude SiStripPayloadCopyAndExclude.cc SiStripMiscellanea/SiStripPayloadCopyAndExclude/plugins/SiStripPayloadCopyAndExclude.cc
+
+ Description: This module is meant to copy the content of a SiStrip APV Gain payload (either G1 or G2) from a local sqlite file (that should be feeded to the Event Setup via the SiStripApvGain3Rcd and put in another local sqlite file, excepted for the modules specified in the excludedModules parameter. If the doReverse parameter is true, the opposite action is performed. 
+
+ Implementation: The implemenation takes advantage of the convenience record SiStripApvGain3Rcd in the EventSetup to be able to hold at the same time two instances of the Strip Gains in the same job.
+
+*/
+//
+// Original Author:  Marco Musich
+//         Created:  Fri, 08 Jun 2018 08:28:01 GMT
+//
+//
+
+// system include files
+#include <memory>
+#include <iostream>
+
+// user include files
+#include "CLHEP/Random/RandGauss.h"
+#include "CalibFormats/SiStripObjects/interface/SiStripGain.h"
+#include "CalibTracker/Records/interface/SiStripGainRcd.h"
+#include "CommonTools/TrackerMap/interface/TrackerMap.h"
+#include "CondCore/DBOutputService/interface/PoolDBOutputService.h"
+#include "CondFormats/DataRecord/interface/SiStripApvGainRcd.h"
+#include "CondFormats/SiStripObjects/interface/SiStripApvGain.h"
+#include "CondFormats/SiStripObjects/interface/SiStripSummary.h"
+#include "DataFormats/SiStripDetId/interface/SiStripDetId.h" 
+#include "DataFormats/SiStripDetId/interface/StripSubdetector.h" 
+#include "DataFormats/TrackerCommon/interface/TrackerTopology.h" 
+#include "FWCore/Framework/interface/ESHandle.h"
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/EventSetup.h"
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+#include "FWCore/Framework/interface/one/EDAnalyzer.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "FWCore/ServiceRegistry/interface/Service.h"
+#include "Geometry/Records/interface/TrackerTopologyRcd.h" 
+
+//
+// class declaration
+//
+
+// If the analyzer does not use TFileService, please remove
+// the template argument to the base class so the class inherits
+// from  edm::one::EDAnalyzer<>
+// This will improve performance in multithreaded jobs.
+
+class SiStripPayloadCopyAndExclude : public edm::one::EDAnalyzer<edm::one::SharedResources>  {
+   public:
+      explicit SiStripPayloadCopyAndExclude(const edm::ParameterSet&);
+      ~SiStripPayloadCopyAndExclude();
+
+      static void fillDescriptions(edm::ConfigurationDescriptions& descriptions);
+
+
+   private:
+      virtual void beginJob() override;
+      virtual void analyze(const edm::Event&, const edm::EventSetup&) override;
+      virtual void endJob() override;
+      std::unique_ptr<SiStripApvGain> getNewObject(const std::map<std::pair<uint32_t,int>,float>& theMap);
+
+      // ----------member data ---------------------------
+      std::vector<unsigned int> m_excludedMods;
+      const std::string m_Record;  
+      const uint32_t m_gainType;
+      const bool m_reverseSelect;
+};
+
+//
+// constants, enums and typedefs
+//
+
+//
+// static data member definitions
+//
+
+//
+// constructors and destructor
+//
+SiStripPayloadCopyAndExclude::SiStripPayloadCopyAndExclude(const edm::ParameterSet& iConfig):
+  m_excludedMods{iConfig.getUntrackedParameter<std::vector<unsigned int> >("excludedModules")},
+  m_Record{iConfig.getUntrackedParameter<std::string> ("record" , "SiStripApvGainRcd")},
+  m_gainType{iConfig.getUntrackedParameter<uint32_t>("gainType",1)},
+  m_reverseSelect{iConfig.getUntrackedParameter<bool>("reverseSelection",false)}
+{
+   //now do what ever initialization is needed
+  sort(m_excludedMods.begin(),m_excludedMods.end());
+
+  edm::LogInfo("ExcludedModules") << "Selected module list";
+  for(std::vector<unsigned int>::const_iterator mod = m_excludedMods.begin();mod!=m_excludedMods.end();mod++) {
+    edm::LogVerbatim("ExcludedModules") << *mod ;
+  }
+
+}
+
+
+SiStripPayloadCopyAndExclude::~SiStripPayloadCopyAndExclude()
+{
+
+   // do anything here that needs to be done at desctruction time
+   // (e.g. close files, deallocate resources etc.)
+
+}
+
+
+//
+// member functions
+//
+
+// ------------ method called for each event  ------------
+void
+SiStripPayloadCopyAndExclude::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
+{
+  using namespace edm;
+
+  // gain to be validated
+  edm::ESHandle<SiStripApvGain> gNew; iSetup.get<SiStripApvGain3Rcd>().get(gNew);
+  edm::ESHandle<SiStripGain> gOld; iSetup.get<SiStripGainRcd>().get(gOld);
+
+  std::map<std::pair<uint32_t,int>,float> theMap,oldPayloadMap;
+ 
+  std::vector<uint32_t> detid;
+  gNew->getDetIds(detid);
+  for (const auto & d : detid) {
+    SiStripApvGain::Range range_new=gNew->getRange(d);
+    SiStripApvGain::Range range_old=gOld->getRange(d,m_gainType);
+    float nAPV=0;
+    
+    for(int it=0;it<range_new.second-range_new.first;it++){
+      nAPV+=1;
+      float Gain=gNew->getApvGain(it,range_new);
+      float patchGain = gOld->getApvGain(it,range_old);
+      std::pair<uint32_t,int> index = std::make_pair(d,nAPV);
+
+      oldPayloadMap[index]=Gain;
+       
+      bool found(false);
+      for(const auto &mod : m_excludedMods) {
+	if(d == mod ) {
+	  edm::LogInfo("ModuleFound") << " module " << mod << " found! Excluded... " << std::endl;
+	  found=true;
+	  break;
+	}
+      }
+
+      if(m_reverseSelect) found =(!found);
+
+      if(!found){
+ 	theMap[index]=Gain;
+      } else {
+	theMap[index]=patchGain;	
+      }
+       
+    } // loop over APVs
+  } // loop over DetIds
+
+  std::unique_ptr<SiStripApvGain> theAPVGains = this->getNewObject(theMap);
+
+  // write out the APVGains record
+  edm::Service<cond::service::PoolDBOutputService> poolDbService;
+  
+  if( poolDbService.isAvailable() )
+    poolDbService->writeOne(theAPVGains.get(),poolDbService->currentTime(),m_Record);
+  else
+    throw std::runtime_error("PoolDBService required.");
+
+}
+
+//********************************************************************************//
+std::unique_ptr<SiStripApvGain>
+SiStripPayloadCopyAndExclude::getNewObject(const std::map<std::pair<uint32_t,int>,float>& theMap) 
+{
+  std::unique_ptr<SiStripApvGain> obj = std::unique_ptr<SiStripApvGain>(new SiStripApvGain());
+  
+  std::vector<float> theSiStripVector;
+  uint32_t PreviousDetId = 0; 
+  for(const auto &element : theMap){
+    uint32_t DetId = element.first.first;
+    if(DetId != PreviousDetId){
+      if(!theSiStripVector.empty()){
+	SiStripApvGain::Range range(theSiStripVector.begin(),theSiStripVector.end());
+	if ( !obj->put(PreviousDetId,range) )  printf("Bug to put detId = %i\n",PreviousDetId);
+      }
+      theSiStripVector.clear();
+      PreviousDetId = DetId;
+    }
+    theSiStripVector.push_back(element.second);
+    
+    edm::LogInfo("SiStripChannelGainFromDBMiscalibrator")<<" DetId: "<<DetId 
+							 <<" APV:   "<<element.first.second
+							 <<" Gain:  "<<element.second
+							 <<std::endl;
+  }
+  
+  if(!theSiStripVector.empty()){
+    SiStripApvGain::Range range(theSiStripVector.begin(),theSiStripVector.end());
+    if ( !obj->put(PreviousDetId,range) )  printf("Bug to put detId = %i\n",PreviousDetId);
+  }
+  
+  return obj;
+}
+
+// ------------ method called once each job just before starting event loop  ------------
+void
+SiStripPayloadCopyAndExclude::beginJob()
+{
+}
+
+// ------------ method called once each job just after ending the event loop  ------------
+void
+SiStripPayloadCopyAndExclude::endJob()
+{
+}
+
+// ------------ method fills 'descriptions' with the allowed parameters for the module  ------------
+void
+SiStripPayloadCopyAndExclude::fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
+  //The following says we do not know what parameters are allowed so do no validation
+  // Please change this to state exactly what you do use, even if it is no parameters
+  edm::ParameterSetDescription desc;
+  desc.setUnknown();
+  descriptions.addDefault(desc);
+
+  //Specify that only 'tracks' is allowed
+  //To use, remove the default given above and uncomment below
+  //ParameterSetDescription desc;
+  //desc.addUntracked<edm::InputTag>("tracks","ctfWithMaterialTracks");
+  //descriptions.addDefault(desc);
+}
+
+//define this as a plug-in
+DEFINE_FWK_MODULE(SiStripPayloadCopyAndExclude);
